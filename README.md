877cc069945ab8f854306c16aa173af7ea2ca59d
kamal <kamaluiuxd@gmail.com>
2022-09-08 14:10:29 +0530

    Installed Tailwind CSS

--- README.md
+++ README.md
@@ -3,19 +3,3 @@
 # Primary : #1E1D35
 
 # Secondary : #47ACEC
-
-# git remote add origin https://github.com/kamaluiuxd/kamal-portfolio.git
-
-# I Create my portfolio website with React JS, Tailwind CSS, Material UI
-
-# echo "# kamal-portfolio" >> README.md
-
-# git init
-
-# git commit -m "first commit"
-
-# git branch -M main
-
-# git remote add origin https://github.com/kamaluiuxd/kamal-portfolio.git
-
-# git push -u origin main
--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 
 function App() {
-	NavLinks = [
-		{
-			link: Home,
-		},
+	const NavLinks = [
+		{ id: 1, link: "Home" },
+		{ id: 2, link: "About" },
+		{ id: 3, link: "Portfolio" },
+		{ id: 4, link: "Projects" },
+		{ id: 5, link: "Contact" },
 	];
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<div>Logo</div>
+			<ul>
+				{NavLinks.map(({ id, link }) => (
+					<li key={id}>{link}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
